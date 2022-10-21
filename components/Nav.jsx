967983021a96bd19cfc15ed69a1809c74ba3894e
Nguyen Duc Tien <neittien0110@gmail.com>
2022-10-21 22:26:35 +0700

    Sắp xếp lại thanh Nav và di chuyển chức năng kiểm tra tài khoản đăng nhập vào Nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,27 +5,46 @@ import { userService } from 'services';
 
 export { Nav };
 
+/**
+ * Thanh NAV, kiêm nhiệm vụ xác thực tài khoản
+ * @returns 
+ */
 function Nav() {
+
+    // Biến trạng thái user 
     const [user, setUser] = useState(null);
 
+    // Tự động chạy khi Nav được khởi tạo
     useEffect(() => {
         const subscription = userService.user.subscribe(x => setUser(x));
+        console.log("Kiểm tra thông tin tài khoản");
         return () => subscription.unsubscribe();
     }, []);
 
+    /**
+     * Hàm sự kiện khi bấm vào nút Đăng xuất trên thanh Nav
+     */
     function logout() {
         userService.logout();
     }
 
-    // only show nav when logged in
+    /** Chỉ hiển thị thanh Nav khi người dùng đã đăng nhập */
     if (!user) return null;
     
     return (
         <nav className="navbar navbar-expand navbar-dark bg-dark">
-            <div className="navbar-nav">
-                <NavLink href="/" exact className="nav-item nav-link">Trang chủ</NavLink>
-                <NavLink href="/users" className="nav-item nav-link">Quản lý tài khoản</NavLink>
-                <a onClick={logout} className="nav-item nav-link">Đăng xuất</a>
+            <div class="container-fluid">
+                <div className="navbar-nav me-auto mb-10 mb-lg-10">
+                    <NavLink href="/" exact className="nav-item nav-link">Trang chủ</NavLink>
+                </div>
+                <div className="navbar-nav">
+                    <NavLink href="/users" className="nav-item nav-link">Quản lý tài khoản</NavLink>
+                    <span style={{color:'white'}}> . </span>
+                    {/** Vừa hiển thị tài khoản, vừa kích hoạt việc kiểm tra trạng thái đăng nhập để gọi tới giao diện login */}
+                    <NavLink href={"/users/edit/" + userService.userValue?.id} className="nav-item nav-link ">Chào {userService.userValue?.lastName + " " + userService.userValue?.firstName}</NavLink>
+                    <span style={{color:'white'}}> . </span>
+                    <a onClick={logout} className="nav-item nav-link">Đăng xuất</a>                    
+                </div>
             </div>
         </nav>
     );
--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,6 @@ function Home() {
     return (
         <div className="p-4">
             <div className="container">
-                <h1>Chào {userService.userValue?.firstName}!</h1>
                 <p>Bạn đang sử dụng sản phẩm trích xuất báo cáo từ hệ thống HIS!!</p>
                 <p><Link href="/users">Quản lý tài khoản đăng nhập</Link></p>
             </div>
